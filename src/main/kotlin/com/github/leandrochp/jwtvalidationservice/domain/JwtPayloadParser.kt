d46b3815f3e45a74ecfc4bebbf6e6befa2130ee8
Leandro Pereira <leandrochp@gmail.com>
2024-03-05 22:58:40 -0300

    refactor: add com.auth0.jwt to validate token format

--- src/main/kotlin/com/github/leandrochp/jwtvalidationservice/domain/JwtPayloadParser.kt
+++ src/main/kotlin/com/github/leandrochp/jwtvalidationservice/domain/JwtPayloadParser.kt
@@ -1,9 +1,11 @@
 package com.github.leandrochp.jwtvalidationservice.domain
 
+import com.auth0.jwt.JWT
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.stereotype.Component
 import java.util.*
+
 
 @Component
 class JwtPayloadParser(
@@ -11,10 +13,11 @@ class JwtPayloadParser(
 ) {
 
     fun valueOf(token: String): JwtPayload = try {
+        val validToken = JWT.decode(token)
         val charset = charset("UTF-8")
-        val parts = token.split(".")
-        val payload = String(Base64.getDecoder().decode(parts[1].toByteArray(charset)), charset)
-
+        val payload = String(
+            Base64.getDecoder().decode(validToken.payload.toByteArray(charset)), charset
+        )
         objectMapper.enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
         objectMapper.readValue(payload, JwtPayload::class.java)
     } catch (e: Exception) {
