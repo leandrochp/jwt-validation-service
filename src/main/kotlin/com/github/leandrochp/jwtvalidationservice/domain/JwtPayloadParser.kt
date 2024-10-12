508bdee6362e90662c0c4feba1fce0d2ac23fe1e
Leandro Pereira <leandrochp@gmail.com>
2024-10-12 19:41:28 -0300

    refactor: refactor code

--- src/main/kotlin/com/github/leandrochp/jwtvalidationservice/domain/JwtPayloadParser.kt
+++ src/main/kotlin/com/github/leandrochp/jwtvalidationservice/domain/JwtPayloadParser.kt
@@ -6,11 +6,13 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.stereotype.Component
 import java.util.*
 
-
 @Component
 class JwtPayloadParser(
     private val objectMapper: ObjectMapper
 ) {
+    init {
+        objectMapper.enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+    }
 
     fun valueOf(token: String): JwtPayload = try {
         val validToken = JWT.decode(token)
@@ -19,7 +21,6 @@ class JwtPayloadParser(
             Base64.getDecoder().decode(validToken.payload.toByteArray(charset)),
             charset
         )
-        objectMapper.enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
         objectMapper.readValue(payload, JwtPayload::class.java)
     } catch (e: Exception) {
         throw Exception("Error parsing JWT: ${e.message}")
